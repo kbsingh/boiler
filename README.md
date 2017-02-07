f04586f0e3cd5e635dd99be2aabde1e8e6ab6322
Karanbir Singh <kbsingh@karan.org>
2017-02-07 15:31:37 +0000

    setup a base framework

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # boiler
 scheduled jobs to run through some boiler plates / user patterns
+
+add tests to the tests/ directory and chmod +x them, they are 
+run in alphabetical order.
+
+remember to tear-down any scaffolding you might setup for a test
+if the following content is likely to be impacted by the scaffolding
+
+tests are run on a new machine, fresh installed and updated 
+CentOS Linux 7/x86_64 
+
--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-echo 'Running tests'
+echo -e "\n[+] `date` -> Tests $0 starting."
+
+# exit as soon as something breaks
+set -e
+
+for f in `/usr/bin/find tests/* -type f `; do
+  ${f}
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+done
 
 # the exit val from this script determines success / fail for this repo
+# but if anything specific broke, the script should have already
+# exited before getting to this point
+
 exit 0
